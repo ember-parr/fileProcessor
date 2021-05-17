ff0cb7dd3a40a0585c918f92e52595a05ab05a3d
Ember P <ember-devops@gmail.com>
2021-05-17 15:24:05 -0500

    created Metadata class

---
+++ Metadata.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Metadata {
+    
+    public string Provider { get; set; }
+    public string Recepient { get; set; }
+    public DateTime Timestamp { get; set; }
+    public List<string> Tags { get; set; } 
+}
