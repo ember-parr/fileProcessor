ffa35b2eba6f9aff3e8cacf785023f0730f0bb92
Ember P <ember-devops@gmail.com>
2021-05-17 15:25:28 -0500

    created AudioFile class

--- Metadata.cs
+++ Metadata.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections.Generic;
 
 public class Metadata {
-    
+    // provider == practitioner in example
+    // recepient == patient in example
     public string Provider { get; set; }
     public string Recepient { get; set; }
     public DateTime Timestamp { get; set; }
