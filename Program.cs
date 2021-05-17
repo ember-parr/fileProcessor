34d58f025309169fe4091582c9f99d2170c972cf
Ember P <ember-devops@gmail.com>
2021-05-17 14:09:04 -0500

    project initilization

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fileProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's process some files!");
+        }
+    }
+}
