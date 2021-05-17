e3f21a397b8cd8f67469c56823699a8a7b4b08ec
Ember P <ember-devops@gmail.com>
2021-05-17 15:13:22 -0500

    added general instructions for myself

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace fileProcessor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Let's process some files!");
+            // we need to iterate through files in uploaded folder
+            // get metadata file
+            // extract info from metadata file, including audio file info
+
+            // for each audio file: 
+            // -- get absolute path
+            // -- verify the checksum
+            // -- generate unique identifier 
+            // -- compress it
+            // -- create a standalone metadata file
         }
     }
 }
